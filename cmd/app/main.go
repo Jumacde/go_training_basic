f71d6bc0f64b2e068ecc09a7cc8ebb2334a96c45
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2026-03-01 21:48:09 +0100

    add rand_digit.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,6 @@
 package main
 
 func main() {
-	show_text()
+	show_text()       // call and extecute show_text method from the hello_world.goo
+	show_rand_digit() // show a random digit
 }
---
+++ cmd/app/rand_digit.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func show_rand_digit() {
+	fmt.Println("show a random digit: ", rand.Intn(20))
+}

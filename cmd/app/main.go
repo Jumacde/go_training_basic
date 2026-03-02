7c4730190ccb806aefc7ab861fc48398e398b993
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2026-03-02 19:11:20 +0100

    add addition method

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,9 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 	//show_text()       // call and extecute show_text method from the hello_world.goo
 	//show_rand_digit() // show a random digit
 	//test_printfn()
-	test_math()
+	//test_math() // show pi
+	x, y := 7, 17
+	result := addition(x, y)
+	fmt.Printf("%d + %d = %d\n", x, y, result)
 
 }
--- cmd/app/math.go
+++ cmd/app/math.go
@@ -8,3 +8,8 @@ import (
 func test_math() {
 	fmt.Printf("pi is %g", math.Pi)
 }
+
+func addition(x int, y int) int {
+	return x + y
+
+}
